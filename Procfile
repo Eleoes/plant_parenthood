16c1b410c1f84112050a296416356632dbff3144
eleoes <leo.escaroz@gmail.com>
2022-11-02 20:05:57 -0700

    wip: deployment to railway

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate gunicorn plantparenthood.wsgi
+web: python manage.py migrate && gunicorn plantparenthood.wsgi
--- plantparenthood/settings.py
+++ plantparenthood/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
@@ -145,3 +146,8 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 # Configure Django App for Heroku
 import django_heroku
 django_heroku.settings(locals())
+
+# Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
